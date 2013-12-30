4d96df31af39697d446827e62ed8d1edd78d1039
benoni <benoni.silva@gmail.com>
2013-12-30 08:00:17 -0200

    Update Comandos.java

--- src/com/example/monografiassh2013/conexao/Comandos.java
+++ src/com/example/monografiassh2013/conexao/Comandos.java
@@ -6,7 +6,7 @@ package com.example.monografiassh2013.conexao;
 public final class Comandos {
 	public static final String LIST_DISK = "fdisk  -l | awk '{ print $1 \" \" $2 \" \" $3 \" \" $4}'";
 	public static final String LIST_PROCESSOS = "ps -auxf |  awk '{print $1 \" \"$2 \" \"$3\" \" $4\" \" $10 \" \"$11}'";
-	public static final String LIST_USUARIOS = "python a.py";
+	public static final String LIST_USUARIOS = "who -a";
 	public static final String LIST_REDES = "/sbin/ifconfig";
 	public static final String INFO_SISTEMA = "uname -a | awk '{print \"\t\"$6 \"  \"$7\"\t\"$3 }'";
 	public static final String INFO_MEMORIA = "free -m | awk '{print $1 \"   \"$2\"   \"$3\" }'";
