a66e574cacf4123cfa5a70f4e2eef020297669c9
Benoni <benonisilva@gmail.com>
2014-01-14 08:28:32 -0300

    mudanca no layout memoria

--- src/com/example/monografiassh2013/MemoriaActivity.java
+++ src/com/example/monografiassh2013/MemoriaActivity.java
@@ -1,24 +1,46 @@
 package com.example.monografiassh2013;
 
 import android.os.Bundle;
+import android.app.Activity;
 import android.app.ListActivity;
 import android.content.Intent;
+import android.util.Log;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
-public class MemoriaActivity extends ListActivity {
+public class MemoriaActivity extends Activity {
 
 	private String[] informacoes;
+	private String[] inf= new String[6];
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_memoria);
 		
-		Intent i = getIntent();
-		informacoes = (String[]) i.getStringArrayExtra("mem");
-		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-		        android.R.layout.test_list_item, informacoes);
-		    setListAdapter(adapter);
+		Intent intent = getIntent();
+		informacoes = (String[]) intent.getStringArrayExtra("mem");
+		informacoes[1] = informacoes[1].replace("Mem: ", "");
+		informacoes[0] = informacoes[0].replace("cached", "");
+		int i=0;
+		for(String s:informacoes[1].split(" ")) {
+			inf[i]=s;
+			i++;
+			
+		}
+		//informacoes[0] = informacoes[0].replace(" ", "      ");
+		//informacoes[1] = informacoes[1].replace(" ", "      ");
+		//informacoes[2]=null;
+		//informacoes[3]=null;
+		TextView tx1 = (TextView) findViewById(R.id.textView5);
+		TextView tx2 = (TextView) findViewById(R.id.textView6);
+		TextView tx3 = (TextView) findViewById(R.id.textView7);
+		TextView tx4 = (TextView) findViewById(R.id.textView8);
+		tx1.setText(inf[0]);
+		tx2.setText(inf[1]);
+		tx3.setText(inf[2]);
+		tx4.setText(inf[3]);
+		
 		
 	}
 
--- src/com/example/monografiassh2013/conexao/Comandos.java
+++ src/com/example/monografiassh2013/conexao/Comandos.java
@@ -10,7 +10,7 @@ public final class Comandos {
 	public static final String LIST_REDES = "/sbin/ifconfig";
 	public static final String INFO_SISTEMA = "uname -a | awk '{print $1\"\t\" $2\"\t\" $3\"\t\" $4\"\t\" $5\" \" $6\" \" $7\" \" $8\" \" $9\" " +
 			" \" $10\" \" $11\" \" $12\"\t\" $13\"\t\" $14\"\t\" $15\"\t\" $16\"\t\" $17\"\t\"}'";
-	public static final String INFO_MEMORIA = "free -m";
+	public static final String INFO_MEMORIA = "free -m | awk '{print $1 \" \"$2 \" \" $3 \" \" $4 \" \" $5 \" \"$6 \" \"}'";
 	public static final String DESLIGA = "shutdown -h now";
 	public static final String REINICIAR = "shutdown -r now";
 }
